6492581096b5e5bf3743944d45ffbcde9191b2c6
chemceem <chemceem@gmail.com>
2015-03-20 00:58:59 -0400

    initial commit. pull from comp397 mail pilot

---
+++ SideScroller/Scripts/objects/car.ts
@@ -0,0 +1,27 @@
+﻿
+module objects {
+    // PLANE CLASS
+    export class Car extends createjs.Bitmap {
+        public width: number;
+        public height: number;
+        // CONSTRUCTOR
+        constructor() {
+            super(assetLoader.getResult("car"));
+
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+
+            this.y = 430;
+            this.regX = this.width * 0.5;
+            this.regY = this.height * 0.5;
+            createjs.Sound.play("engine", {loop: -1});
+        }
+
+        // PUBLIC METHODS
+        public update() {
+            this.x = stage.mouseX;
+        }
+
+    }
+
+} 
---
+++ SideScroller/Scripts/objects/road.js
@@ -0,0 +1,40 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var objects;
+(function (objects) {
+    // OCEAN CLASS
+    var Road = (function (_super) {
+        __extends(Road, _super);
+        // CONSTRUCTOR
+        function Road() {
+            _super.call(this, assetLoader.getResult("road"));
+            // PUBLIC INSTANCE VARIABLES
+            this._dy = 5;
+            this.reset();
+        }
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+        Road.prototype.update = function () {
+            this.y += this._dy;
+            this._checkBounds();
+        };
+        // Reset position of island to the top
+        Road.prototype.reset = function () {
+            this.y = -960;
+            this.x = 0;
+        };
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++
+        Road.prototype._checkBounds = function () {
+            // check if island has left the bottom of the screen
+            if (this.y === 0) {
+                this.reset();
+            }
+        };
+        return Road;
+    })(createjs.Bitmap);
+    objects.Road = Road;
+})(objects || (objects = {}));
+//# sourceMappingURL=road.js.map
